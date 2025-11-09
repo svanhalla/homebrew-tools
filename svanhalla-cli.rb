feb25f9c6e0fbc45d71ed5b1114c06ef3aba7592
hans.bringert <hans.bringert@gmail.com>
2025-11-09 20:44:21 +0100

    Update svanhalla-cli to v1.0.9 with centralized releases

--- svanhalla-cli.rb
+++ svanhalla-cli.rb
@@ -1,17 +1,23 @@
 class SvanhallaCli < Formula
   desc "Svanhalla CLI reklamprogram"
-  homepage "https://github.com/svanhalla/svanhalla-cli"
-  head "https://github.com/svanhalla/svanhalla-cli.git", branch: "main"
-  license "MIT"
-
-  depends_on "go" => :build
+  homepage "https://github.com/svanhalla/svanhalla-releases"
+  version "1.0.9"
+  
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/svanhalla/svanhalla-releases/releases/download/svanhalla-cli-v1.0.9/svanhalla-cli-darwin-arm64"
+      sha256 "8fe2b7e1bc7759c507fc12ca93819ada8800afcda782f0ec7a37efbcf5e3923b"
+    else
+      url "https://github.com/svanhalla/svanhalla-releases/releases/download/svanhalla-cli-v1.0.9/svanhalla-cli-darwin-amd64"
+      sha256 "a2b09d95bfb1906b7d51c0a660510b970ce916526e7e9ac9ba233d1add295ef3"
+    end
+  end
 
   def install
-    system "make", "build"
-    bin.install "bin/svanhalla-cli"
+    bin.install "svanhalla-cli-darwin-#{Hardware::CPU.arch}" => "svanhalla-cli"
   end
 
   test do
-    assert_match "svanhalla-cli", shell_output("#{bin}/svanhalla-cli version")
+    assert_match "svanhalla-cli v1.0.9", shell_output("#{bin}/svanhalla-cli version")
   end
 end
