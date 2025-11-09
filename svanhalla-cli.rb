9d1ce1f63ba2ddf46c96d27e4e4f6a9ee47bf6e6
hans.bringert <hans.bringert@gmail.com>
2025-11-09 20:50:47 +0100

    Update svanhalla-cli to v1.1.0

--- svanhalla-cli.rb
+++ svanhalla-cli.rb
@@ -1,15 +1,15 @@
 class SvanhallaCli < Formula
   desc "Svanhalla CLI reklamprogram"
   homepage "https://github.com/svanhalla/svanhalla-releases"
-  version "1.0.9"
+  version "1.1.0"
   
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/svanhalla/svanhalla-releases/releases/download/svanhalla-cli-v1.0.9/svanhalla-cli-darwin-arm64"
-      sha256 "8fe2b7e1bc7759c507fc12ca93819ada8800afcda782f0ec7a37efbcf5e3923b"
+      url "https://github.com/svanhalla/svanhalla-releases/releases/download/svanhalla-cli-v1.1.0/svanhalla-cli-darwin-arm64"
+      sha256 "20a4321e45aa5c9f52ddfc125c0a61f9f60b3e691a3165f08d711609981d8ce7"
     else
-      url "https://github.com/svanhalla/svanhalla-releases/releases/download/svanhalla-cli-v1.0.9/svanhalla-cli-darwin-amd64"
-      sha256 "a2b09d95bfb1906b7d51c0a660510b970ce916526e7e9ac9ba233d1add295ef3"
+      url "https://github.com/svanhalla/svanhalla-releases/releases/download/svanhalla-cli-v1.1.0/svanhalla-cli-darwin-amd64"
+      sha256 "9d8ad4db976561ad9b2b0a5d3708460204c660c4234aed3dbcc8665be1a9c9ca"
     end
   end
 
@@ -18,6 +18,6 @@ class SvanhallaCli < Formula
   end
 
   test do
-    assert_match "svanhalla-cli v1.0.9", shell_output("#{bin}/svanhalla-cli version")
+    assert_match "svanhalla-cli v1.1.0", shell_output("#{bin}/svanhalla-cli version")
   end
 end
