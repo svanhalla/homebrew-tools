dc56061218e4f7ca31eb13844cca80e12ee92617
hans.bringert <hans.bringert@gmail.com>
2025-11-10 19:56:17 +0100

    Fix disabled Formula syntax - add required URL

--- svanhalla-cli.rb
+++ svanhalla-cli.rb
@@ -4,6 +4,7 @@
 class SvanhallaCli < Formula
   desc "Svanhalla CLI reklamprogram"
   homepage "https://github.com/svanhalla/svanhalla-releases"
+  url "https://github.com/svanhalla/svanhalla-releases/archive/v1.2.9.tar.gz"
   version "1.2.9"
 
   disable! date: "2025-11-10", because: "the cask should be used now instead", replacement_cask: "svanhalla-cli"
